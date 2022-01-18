ffe44fce99d6ef23e5cd70c37088eaf107bd8528
stas-kuznetsov <89393832+stas-kuznetsov@users.noreply.github.com>
2022-01-18 11:05:38 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ username=root
 
 ssh -i /key/id_rsa $username@$IPadr << EOF
 docker pull staskuznetsov/app1:$1
-docker ps -q | docker rm
+# docker ps -q | docker rm
 docker run -d -p 8080:8080 staskuznetsov/app1:$1
 EOF
