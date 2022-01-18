562d3381dc69bdde13eb18ee822f29551f7e3539
stas-kuznetsov <89393832+stas-kuznetsov@users.noreply.github.com>
2022-01-18 11:58:47 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ username=root
 #ssh -i /key/id_rsa $username@$IPadr << EOF
 #docker pull staskuznetsov/app1:$1
 # docker ps -q | docker rm
-#docker run -d -p 8080:8080 staskuznetsov/app1:$1
+#docker run -d -p 8080:8080 staskuznetsov/petclinic:$1
 #EOF
 
 
 ssh -i /key/id_rsa $username@$IPadr "$( cat <<'EOT'
 docker pull staskuznetsov/app1:$1
 # docker ps -q | docker rm
-docker run -d -p 8080:8080 staskuznetsov/app1:$1
+docker run -d -p 8080:8080 staskuznetsov/petclinic:$1
 EOT
 )"
